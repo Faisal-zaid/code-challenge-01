2ae42b97b6a8faa7696befb036ff0370b34b8158
Faisal Zaid <zaidalideveloper@gmail.com>
2025-09-25 15:33:20 +0300

    formatted

--- netsalarycalculator.js
+++ netsalarycalculator.js
@@ -1,59 +1,50 @@
-function malipo(basicSalary, benefits){
+function malipo(basicSalary, benefits) {
+  //const basicSalary = 50000;
+  //const benefits = 10000;
 
-//const basicSalary = 50000;
-//const benefits = 10000;
+  glossSalary = basicSalary + benefits;
+  //glossSalary has been gotten by adding basicSalary and benefits
 
-glossSalary=basicSalary + benefits
-//glossSalary has been gotten by adding basicSalary and benefits
+  let payee;
+  if (glossSalary < 0) {
+    payee = "not eligible to pay";
+  }
+  //this part above ensures people not earning anything dont pay tax
+  else if (glossSalary <= 24000) {
+    payee = (10 / 100) * glossSalary;
+  } else if (glossSalary <= 32333) {
+    payee = (25 / 100) * glossSalary;
+  } else if (glossSalary <= 500000) {
+    payee = (30 / 100) * glossSalary;
+  } else if (glossSalary <= 800000) {
+    payee = (32.5 / 100) * glossSalary;
+  } else if (glossSalary > 800000) {
+    payee = (35 / 100) * glossSalary;
+  }
+  //the arguments above are to calculate the payee one is eligible to pay
 
-let payee;
-if (glossSalary <0){
-    payee='not eligible to pay'
-}
-//this part above ensures people not earning anything dont pay tax
-else if(glossSalary<=24000){
-    payee=(10/100*glossSalary) 
-}
-else if(glossSalary<=32333){
-    payee=(25/100*glossSalary) 
-}
-else if(glossSalary<=500000){
-    payee=(30/100*glossSalary) 
-}
-else if(glossSalary<=800000){
-    payee=(32.5/100*glossSalary) 
-}
-else if(glossSalary>800000){
-    payee=(35/100*glossSalary) 
-}
-//the arguments above are to calculate the payee one is eligible to pay
+  let nhif;
+  nhif = (2.75 / 100) * glossSalary;
 
-let nhif;
-nhif = 2.75/100 *glossSalary
+  //above is a calculation for 2.75% on glosss salary
+  let nssf;
+  if (glossSalary <= 8000) {
+    nssf = (6 / 100) * basicSalary;
+  } else if (glossSalary <= 72000) {
+    nssf = (6 / 100) * basicSalary;
+  } else {
+    nssf = (6 / 100) * 72000;
+  }
+  //calculates the nssf eligible to be payed
+  let netSalary;
 
-//above is a calculation for 2.75% on glosss salary
-let nssf;
-if (glossSalary <= 8000){
-    nssf = 6/100 * basicSalary
-}
-else if (glossSalary <= 72000){
-    nssf =6/100 * basicSalary
-}
-else{
-    nssf =6/100*72000
-}
-//calculates the nssf eligible to be payed
-let netSalary;
+  deductions = nssf + nhif + payee;
+  netSalary = glossSalary - deductions;
 
-deductions=nssf+nhif+payee
-netSalary=glossSalary-(deductions);
-
-return {basicSalary, benefits,payee,glossSalary,nhif,nssf,netSalary};
-
+  return { basicSalary, benefits, payee, glossSalary, nhif, nssf, netSalary };
 }
 
-
-console.log(malipo(5000, 1000))
+console.log(malipo(5000, 1000));
 
 //calculates net salary by multiplying basic salary by 12 months
 //console.log(payee)
--- speeddetector.js
+++ speeddetector.js
@@ -1,33 +1,30 @@
 //added currentSpeed variable to be able to follow up the code
-function speedCalculator(speed){
+function speedCalculator(speed) {
+  const speedLimit = 70;
+  const kmPerPoint = 5;
+  //let speed ;
 
-const speedLimit = 70;
-const kmPerPoint =5;
-//let speed ;
-
-
-let points;
-// the argument below suggests if the currentspeed is within the required speed
-//the points should be returned as ok
-if (speed<=speedLimit){
-   points='ok'
-}
-//it further ahead states that if its above the required speed it should add a 
-//point every 5km/hr more of speed
-else {
-     points=((speed - speedLimit)/5)
-}
-//the function here confirms if points exceed the set amount the license gets suspended
-if (points >12){
-    points=("License suspended")
-}
-//this function calls for the points
-//else{
+  let points;
+  // the argument below suggests if the currentspeed is within the required speed
+  //the points should be returned as ok
+  if (speed <= speedLimit) {
+    points = "ok";
+  }
+  //it further ahead states that if its above the required speed it should add a
+  //point every 5km/hr more of speed
+  else {
+    points = (speed - speedLimit) / 5;
+  }
+  //the function here confirms if points exceed the set amount the license gets suspended
+  if (points > 12) {
+    points = "License suspended";
+  }
+  //this function calls for the points
+  //else{
   //  console.log(points)
-//}
+  //}
 
-return points;
-
+  return points;
 }
 // the points are printed
 console.log(speedCalculator(80));
